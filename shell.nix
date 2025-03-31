80f68973134a5f9dd57e2275b3b9fcc17d859c8b
Roger Steve Ruiz <hi@rog.gr>
2025-03-31 18:51:56 -0500

    Renombra el proyecto de `gen-social` a `socio`

--- shell.nix
+++ shell.nix
@@ -4,43 +4,44 @@ let
   pythonPackages = python311Packages;
 in
 
-pkgs.mkShell /* rec */ {
-  name = "gen-social";
-  venvDir = "./.venv";
-  buildInputs = [
-    pythonPackages.python
-    pythonPackages.venvShellHook
-    # pythonPackages.click
-    # pythonPackages.wand
-    # pythonPackages.pydantic
-    pythonPackages.build
-    pythonPackages.setuptools
+pkgs.mkShell # rec
+  {
+    name = "socio";
+    venvDir = "./.venv";
+    buildInputs = [
+      pythonPackages.python
+      pythonPackages.venvShellHook
+      # pythonPackages.click
+      # pythonPackages.wand
+      # pythonPackages.pydantic
+      pythonPackages.build
+      pythonPackages.setuptools
 
-    pythonPackages.python-lsp-server
+      pythonPackages.python-lsp-server
 
-    imagemagick
-  ];
-
-  FONTCONFIG_FILE = makeFontsConf {
-    fontDirectories = with pkgs; [
-      (google-fonts.override {
-        fonts = [
-          "Quattrocento"
-          "Abril Fatface"
-        ];
-      })
-      (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+      imagemagick
     ];
-  };
 
-  postVenvCreation = ''
-    unset SOURCE_DATE_EPOCH
-    pip install -r requirements.txt
-  '';
+    FONTCONFIG_FILE = makeFontsConf {
+      fontDirectories = with pkgs; [
+        (google-fonts.override {
+          fonts = [
+            "Quattrocento"
+            "Abril Fatface"
+          ];
+        })
+        (nerdfonts.override { fonts = [ "Recursive" ]; })
+      ];
+    };
 
-  postShellHook = ''
-    # allow pip to install wheels
-    unset SOURCE_DATE_EPOCH
-    export MAGICK_HOME="${pkgs.imagemagick}"
-  '';
-}
+    postVenvCreation = ''
+      unset SOURCE_DATE_EPOCH
+      pip install -r requirements.txt
+    '';
+
+    postShellHook = ''
+      # allow pip to install wheels
+      unset SOURCE_DATE_EPOCH
+      export MAGICK_HOME="${pkgs.imagemagick}"
+    '';
+  }
